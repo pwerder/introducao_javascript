90661dd9df78eca9ff276cab955090debcb5b8c4
Paulo Antonio <paulo.werder@gmail.com>
2020-04-24 16:24:26 -0300

    commit inicial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert("meu primeiro JS");
+
+let idade = 31;
+let nome = "Paulo Antonio";
+
+let frase = "Japao e o melhor time do mundo";
+
+console.log(frase.replace("Japao", "Brasil"));
+console.log(frase.toLowerCase());
+
+
+alert("Nome: " + nome + " idade: " + idade);
