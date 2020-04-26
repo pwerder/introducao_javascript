a16342efa02bb536ca41e1223905611bd6d09cf4
Paulo Antonio <paulo.werder@gmail.com>
2020-04-26 17:21:41 -0300

    replace

--- js/main.js
+++ js/main.js
@@ -1,28 +1,11 @@
-//date
-var d = new Date();
-alert(d.getDate());
+function soma(n1, n2){
+    return n1 + n2; 
+}
 
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
 
+alert(soma(5,10));
 
-// //dicionario
-// let frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxo"}];
-// console.log(frutas);
-// alert(frutas[1].cor);
-
-
-// let fruta = {nome:"maça", cor:"vermelha"}
-// console.log(fruta.nome);
-
-
-// let lista = ['maça', 'pèra', 'laranja'];
-// lista.push("uva");  //adicionando
-// console.log(lista);
-// console.log(lista.join(" - "));
-
-// let idade = 31;
-// let nome = "Paulo Antonio";
-// let frase = "Japao e o melhor time do mundo";
-// console.log(frase.replace("Japao", "Brasil"));
-// console.log(frase.toLowerCase());
-// console.log(frase.toUpperCase());
-// alert("Nome: " + nome + " idade: " + idade);
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
