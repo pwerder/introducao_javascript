c71688568e8342fe3f77e897f3cafd2dde2685e6
Paulo Antonio <paulo.werder@gmail.com>
2020-04-26 17:26:16 -0300

    validar idade

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-function soma(n1, n2){
-    return n1 + n2; 
+function validaIdade(idade){
+    if(idade>=18){
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function setReplace(frase, nome, novo_nome){
-    return frase.replace(nome, novo_nome);
-}
-
-alert(soma(5,10));
-
-alert(setReplace("Vai Japão", "Japão", "Brasil"));
+let idade = prompt("Qual é sua idade?");
+console.log(validaIdade(idade));
